46565bce758c0654fda8c6c4ff2fb9969eb49f8a
Đào Thái Sơn <thaisonn0701@gmail.com>
2023-10-31 17:14:47 +0700

    static file, SCSS and use Bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 8000
 
+app.use(express.static(path.join(__dirname, 'puplic')))
+
 // HTTP logger
 app.use(morgan('combined'))
 
